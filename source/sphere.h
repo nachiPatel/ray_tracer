76a7cd9469f729d0c50297f7b3b62d9841f4e354
Nachiket Patel <nachi.patel97@gmail.com>
2021-01-23 07:51:30 -0500

    added hittable list

--- source/sphere.h
+++ source/sphere.h
@@ -43,7 +43,8 @@ bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) cons
 
     rec.t = root;
     rec.p = r.at(root);
-    rec.normal = (rec.p - centre) / radius;
+    vec3 outward_normal = (rec.p - centre) / radius;
+    rec.set_face_normal(r, outward_normal);
 
     return true;
 }
