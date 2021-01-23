6d5ee86a83c1fee3278a30e0f56ee01ccdb69ddf
Nachiket Patel <nachi.patel97@gmail.com>
2021-01-22 20:06:59 -0500

    added hittable class , simplified the hit function and put it into sphere class

---
+++ source/sphere.h
@@ -0,0 +1,51 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable
+{
+public:
+    sphere() {}
+    sphere(point3 cen, double r) : centre(cen), radius(r) {}
+
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override;
+
+public:
+    point3 centre;
+    double radius;
+};
+
+bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) const
+{
+    vec3 oc = r.origin() - centre;
+    auto a = r.direction().squared_length();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.squared_length() - radius * radius;
+
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0)
+        return false;
+
+    auto sqrtd = sqrt(discriminant);
+
+    //find the nearest root within range
+    auto root = (-half_b - sqrtd) / a;
+    if (root < t_min || t_max < root)
+    {
+        root = (-half_b + sqrtd) / a;
+        if (root < t_min || t_max < root)
+        {
+            return false;
+        }
+    }
+
+    rec.t = root;
+    rec.p = r.at(root);
+    rec.normal = (rec.p - centre) / radius;
+
+    return true;
+}
+
+#endif
