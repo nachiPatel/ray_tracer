0fd601319b6ab6408e56dd3af6ed347b6fbdbef7
Nachiket Patel <nachi.patel97@gmail.com>
2021-01-23 20:37:17 -0500

    changed manin method to use hittable list

--- source/hittable_list.h
+++ source/hittable_list.h
@@ -42,10 +42,10 @@ bool hittable_list::hit(const ray &r, double t_min, double t_max, hit_record &re
 
     for (const auto &object : objects)
     {
-        if (object->hit(r, t_min, t_max, rec))
+        if (object->hit(r, t_min, closest_so_far, temp_rec))
         {
             hit_anything = true;
-            closest_so_far = rec.t;
+            closest_so_far = temp_rec.t;
             rec = temp_rec;
         }
     }
