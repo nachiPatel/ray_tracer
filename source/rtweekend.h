ff2955cdfe8157398c3ac6e613951d48a6c34f3b
Nachiket Patel <nachi.patel97@gmail.com>
2021-01-24 21:55:02 -0500

    added camera class and also changed main method to use camera class

--- source/rtweekend.h
+++ source/rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 //using
 using std::make_shared;
@@ -15,11 +16,31 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 //utility function
-inline double degree_to_radians(double degree)
+double clamp(double x, double min, double max)
+{
+    if (x < min)
+        return min;
+    if (x > max)
+        return max;
+    return x;
+}
+
+double degree_to_radians(double degree)
 {
     return degree * pi / 180.0;
 }
 
+double random_double()
+{
+    // returns random number greater and equeal to 0 or less than 1
+    return rand() / (RAND_MAX + 1.0);
+}
+
+double random_double(double min, double max)
+{
+    //returns random number between min and max
+    return min + (max - min) * random_double();
+}
 //common header
 #include "vec3.h"
 #include "ray.h"
